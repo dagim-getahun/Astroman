9b68c88dfb4d067279fab70c4930acc00af94541
Dagim Getahun <dagim.getahun.abebe@gmail.com>
2023-01-23 16:33:22 +0300

    Vertical jump deceleration effect

--- index.js
+++ index.js
@@ -18,17 +18,29 @@ class PlayerMain{
             y:10
         }
         this.gravity = 0.5
+        this.jumpAcceleration = 4
+        this.jump = false
+        this.jumpHeight = 300
     }
         draw(){
             canv.fillStyle='grey'
             canv.fillRect(this.position.x,this.position.y,this.size.width,this.size.height)
         }
         update(){
-            this.position.y += this.velocity.y
-            if(this.position.y+this.size.height+this.velocity.y <= canvas.height){
-                this.velocity.y += this.gravity
+            if(this.jump){
+                this.position.y -= this.velocity.y
+                if(this.velocity.y > 0 && this.position.y+this.size.height+this.velocity.y >= canvas.height-this.jumpHeight){
+                    this.velocity.y -= this.jumpAcceleration
+                }else{
+                    this.jump=false
+                }
             }else{
-                this.velocity.y = 0
+                this.position.y += this.velocity.y
+                if(this.position.y+this.size.height+this.velocity.y <= canvas.height){
+                    this.velocity.y += this.gravity
+                }else{
+                    this.velocity.y = 0
+                }
             }
             this.draw()
         }
@@ -39,8 +51,16 @@ class PlayerMain{
 
     function animate(){
         requestAnimationFrame(animate)
-        console.log(player.position.y)
         canv.clearRect(0,0,canvas.width,canvas.height)
         player.update()
     }
     animate()
+    
+    addEventListener('keydown',(event)=>{
+        switch(event.keyCode){
+            case 38:
+            player.jump=true
+            player.velocity.y=50
+
+        }
+    })
