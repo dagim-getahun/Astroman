21f469336cfc096b509a386803f982e9aac02a9f
Dagim Getahun <dagim.getahun.abebe@gmail.com>
2023-01-23 17:19:48 +0300

    Platform and Player collision detection

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const canvas = document.querySelector('canvas')
 canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.height = innerHeight/2
 const canv = canvas.getContext('2d')
 
 class PlayerMain{
     constructor(){
         this.position = {
             x:100,
-            y:100
+            y:400
         }
         this.size = {
             width:50,
@@ -20,7 +20,7 @@ class PlayerMain{
         this.gravity = 0.5
         this.jumpAcceleration = 4
         this.jump = false
-        this.jumpHeight = 300
+        this.jumpHeight = this.position.y+300
         this.forward=false
         this.backward=false
     }
@@ -53,14 +53,40 @@ class PlayerMain{
             this.draw()
         }
     }
-    
-    const player = new PlayerMain()
-    // player.update()
 
+    class Platform{
+        constructor(){
+            this.position={
+                x:100,
+                y:200
+            }
+            this.size={
+                width:300,
+                height:20
+            }
+        }
+        draw(){
+            canv.fillStyle='red'
+            canv.fillRect(this.position.x,this.position.y,this.size.width,this.size.height)
+        }
+    }
+
+
+    const player = new PlayerMain()
+    const platform = new Platform()
+    
     function animate(){
-        requestAnimationFrame(animate)
         canv.clearRect(0,0,canvas.width,canvas.height)
         player.update()
+        platform.draw()
+        if(player.position.y+player.size.height <= platform.position.y && 
+           player.position.y+player.size.height+player.velocity.y >= platform.position.y &&
+           player.position.x+player.size.width >= platform.position.x &&
+           player.position.x+player.velocity.x <= platform.position.x+platform.size.width 
+           ){
+            player.velocity.y=0
+        }
+        requestAnimationFrame(animate)
     }
     animate()
     
@@ -68,7 +94,7 @@ class PlayerMain{
         switch(event.keyCode){
             case 38:
                 player.jump=true
-                player.velocity.y=50
+                player.velocity.y += 50
                 break
             case 37:
                 player.backward = true
